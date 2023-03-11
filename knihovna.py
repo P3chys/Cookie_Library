171b95545e7e6b91ce4b48a18548152301643063
P3chys <pechysadam@gmail.com>
2023-03-11 01:41:23 +0100

    Vypujcka old Check Vypujcka for transfer Error catching

--- knihovna.py
+++ knihovna.py
@@ -10,61 +10,19 @@ class Kniha():
         self.pocet = pocet
         self.isbn = isbn
         Kniha.pridejKnihu(self)
-
-    def kontrolaExistence(isbn):
-        select = "SELECT count(*) FROM Kniha WHERE isbn = ?"
-        cursor = conn.cursor()
-        cursor.execute(select,[isbn])
-        pocet = cursor.fetchone()[0]
-        return pocet
     
     def pridejKnihu(self):
         kniha = (self.isbn,self.nazev,self.autor,self.rok,self.pocet)
-        if(Kniha.kontrolaExistence(self.isbn)==0):
+        try:
             insert = "INSERT INTO Kniha (isbn,nazev, autor, rok, pocet) VALUES (?, ?, ?, ?, ?)"
             cursor = conn.cursor()
             cursor.execute(insert,kniha)
             conn.commit()
-            print("Kniha ",self.nazev," pridana ")
+        except:
+            print("Kniha ",self.nazev," jiz existuje v Databazi")
+        else:
+            print("Kniha ",self.nazev," pridana")
 
-    
-    def pridejPocet(self):
-        update = "UPDATE Kniha SET pocet = pocet+1 WHERE isbn = ? "
-        cursor = conn.cursor()
-        cursor.execute(update,[self.isbn])
-        conn.commit()
-
-    def uberPocet(self):
-        update = "UPDATE Kniha SET pocet = pocet-1 WHERE isbn = ?"
-        cursor = conn.cursor()
-        cursor.execute(update,[self.isbn])
-        conn.commit()
-        
-class Zakaznik():
-    def __init__(self,jmeno,prijmeni, rc):
-        self.jmeno = jmeno
-        self.prijmeni = prijmeni
-        self.rc = rc
-        Zakaznik.pridejZakaznika(self)
-
-    def kontrolaExistence(rc):
-        select = "SELECT count(*) FROM Zakaznik WHERE rc=?"
-        cursor = conn.cursor()
-        cursor.execute(select,[rc])
-        pocet = cursor.fetchone()[0]
-        return pocet
-    
-    def pridejZakaznika(self):
-        zakaznik = (self.rc,self.jmeno,self.prijmeni)
-        if(Zakaznik.kontrolaExistence(self.rc)==0):
-            insert = "INSERT INTO Zakaznik (rc, jmeno, prijmeni) VALUES (?, ?, ?)"
-            cursor = conn.cursor()
-            cursor.execute(insert,zakaznik)
-            conn.commit()
-            print("Zakaznik ",self.rc," pridan ")
-
-
-class Vypujcka():
     def kontrolaPoctu(isbn):
         select = "SELECT pocet from Kniha where isbn = ?"
         cursor = conn.cursor()
@@ -72,23 +30,87 @@ class Vypujcka():
         pocet = cursor.fetchone()[0]
         return pocet
     
+    def pridejPocet(isbn):
+        update = "UPDATE Kniha SET pocet = pocet+1 WHERE isbn = ? "
+        cursor = conn.cursor()
+        cursor.execute(update,[isbn])
+        conn.commit()
+        print("pocet zvysen")
 
-    def zalozVypujcku(zakaznik,kniha):
-        if(Vypujcka.kontrolaPoctu(kniha.isbn)>0):
-            insert = "INSERT INTO Vypujcka(isbn,rc) VALUES(?,?)"
+    def uberPocet(isbn):
+        update = "UPDATE Kniha SET pocet = pocet-1 WHERE isbn = ?"
+        cursor = conn.cursor()
+        cursor.execute(update,[isbn])
+        conn.commit()
+        print("pocet snizen")
+        
+class Zakaznik():
+    def __init__(self,jmeno,prijmeni, rc):
+        self.jmeno = jmeno
+        self.prijmeni = prijmeni
+        self.rc = rc
+        Zakaznik.pridejZakaznika(self)
+    
+    def pridejZakaznika(self):
+        try:
+            zakaznik = (self.rc,self.jmeno,self.prijmeni)
+            insert = "INSERT INTO Zakaznik (rc, jmeno, prijmeni) VALUES (?, ?, ?)"
             cursor = conn.cursor()
-            cursor.execute(insert,[kniha.isbn,zakaznik.rc])
+            cursor.execute(insert,zakaznik)
             conn.commit()
-            kniha.uberPocet()
+        except:
+            print("Zakaznik ",self.rc," jiz existuje v Databazi ")
+        else:
+            print("Zakaznik ",self.rc," pridan ")
+
+class Vypujcka():
+    
+    
+    def kontrolaVypujcky(isbn,rc):
+        select = "SELECT COUNT(*) FROM Vypujcka WHERE isbn = ? AND rc = ?"
+        cursor = conn.cursor()
+        cursor.execute(select,(isbn,rc))
+        pocet = cursor.fetchone()[0]
+        return pocet
+
+    def zalozVypujcku(rc,isbn):
+        if(Kniha.kontrolaPoctu(isbn)>0):
+            insert = "INSERT INTO Vypujcka(isbn,rc,datum_vypujceni) VALUES(?,?,DATETIME('now'))"
+            cursor = conn.cursor()
+            cursor.execute(insert,[isbn,rc])
+            conn.commit()
+            Kniha.uberPocet(isbn)
+            print("Vypujcka zalozena")
         else:
             print("Neni dostatek knih")
-                
+
+
+    def uzavriVypujcku(rc, isbn):
+        try:
+            insert = """
+            INSERT INTO Vypujcka_old(isbn,rc,datum_vypujceni) 
+            SELECT isbn, rc, datum_vypujceni FROM Vypujcka WHERE isbn = ? AND rc = ?
+            """
+            cursor = conn.cursor()
+            cursor.execute(insert,(isbn,rc))
+            conn.commit()
+        except:
+            print("Vypujcka teto knihy pro tohoto cloveka neexistuje")
+
+        else:
+            delete = "DELETE FROM Vypujcka WHERE isbn=? AND rc = ?"
+            cursor = conn.cursor()
+            cursor.execute(delete,(isbn, rc))
+            conn.commit()
+            print("Vypujcka uzavrena")
+            Kniha.pridejPocet(isbn)
 
 
 
-k = Kniha("nejakerandomisbn","How to python","Adam",2022,1)
-z = Zakaznik("Adam", "Pech", 9909094349)
+Kniha("nejakerandomisbn","How to python","Adam",2022,1)
+Zakaznik("Adam", "Pech", 9909094349)
+Vypujcka.zalozVypujcku(9909094349,"nejakerandomisbn")
+Vypujcka.uzavriVypujcku(9909094349,"nejakerandomisbn")
 
-Vypujcka.zalozVypujcku(z,k)
 
 
